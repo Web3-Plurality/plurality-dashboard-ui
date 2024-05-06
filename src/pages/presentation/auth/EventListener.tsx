0fc0b5372f0b18fb5c23735733c2c97580b343b7
Hira Siddiqui <12besehsiddiqui@seecs.edu.pk>
2024-05-06 16:02:08 +0200

    feat: add event listeners for web3 transactions

--- src/pages/presentation/auth/EventListener.tsx
+++ src/pages/presentation/auth/EventListener.tsx
@@ -5,15 +5,35 @@ import React, { useEffect } from 'react';
 import { useAccount, useSignMessage } from 'wagmi';
 
 const EventListener: React.FC = () => {
-    /*  const { address } = useAccount();
-    const { data: signedData, isError, isLoading, isSuccess, signMessage } = useSignMessage({
-        message: 'gm wagmi frens',
-      });   */
+    
       const receiveMessage = async (event: MessageEvent) => {
-
-        const parentUrl = "http://localhost:3001";
+          const parentUrl = "http://localhost:3001";
           if (event.origin === parentUrl) {
               const data = event.data;
+              let signer = null;
+
+              let provider;
+              if (window.ethereum == null) {
+                
+                // If MetaMask is not installed, we use the default provider,
+                // which is backed by a variety of third-party services (such
+                // as INFURA). They do not have private keys installed,
+                // so they only have read-only access
+                //console.log("MetaMask not installed; using read-only defaults")
+                //provider = ethers.getDefaultProvider()
+                console.log("Please install metamask");
+                return; //todo: send to parent?
+                
+              } else {
+                
+                // Connect to the MetaMask EIP-1193 object. This is a standard
+                // protocol that allows Ethers access to make all read-only
+                // requests through MetaMask.
+                provider = new ethers.BrowserProvider(window.ethereum);
+                signer = await provider.getSigner();
+
+              }
+        
               if (data.type === 'metamaskRequest' && data.method === 'eth_requestAccounts') {
                   // Call MetaMask method
                   window.ethereum.request({ method: 'eth_requestAccounts' })
@@ -35,7 +55,7 @@ const EventListener: React.FC = () => {
                   // For historical reasons, you must submit the message to sign in hex-encoded UTF-8.
                   // This uses a Node.js-style buffer shim in the browser.
                   // const msg = `0x${btoa(data.message)}`;
-                  console.log("Received personal sign request: " , event.origin);
+                  /*console.log("Received personal sign request: " , event.origin);
                   window.ethereum.request({ method: 'eth_requestAccounts' })
                       .then(accounts => {
                        
@@ -52,38 +72,32 @@ const EventListener: React.FC = () => {
                       })
                       .catch(error => {
                           // Handle error
-                      });
+                      });*/
+                      try {
+                      let signature = await signer.signMessage(data.message);
+                      window.parent.postMessage({ type: 'metamaskResponse', data: signature }, parentUrl);
+                      }
+                      catch (e) {
+                        console.log(e);
+                      }
                   
               }
-              if (data.type === 'metamaskRequest' && data.method === 'balance') {
-                let signer = null;
-
-                let provider;
-                if (window.ethereum == null) {
-                
-                    // If MetaMask is not installed, we use the default provider,
-                    // which is backed by a variety of third-party services (such
-                    // as INFURA). They do not have private keys installed,
-                    // so they only have read-only access
-                    console.log("MetaMask not installed; using read-only defaults")
-                    provider = ethers.getDefaultProvider()
-                
-                } else {
-                
-                    // Connect to the MetaMask EIP-1193 object. This is a standard
-                    // protocol that allows Ethers access to make all read-only
-                    // requests through MetaMask.
-                    provider = new ethers.BrowserProvider(window.ethereum)
-                
-                    // It also provides an opportunity to request access to write
-                    // operations, which will be performed by the private key
-                    // that MetaMask manages for the user.
-                    signer = await provider.getSigner();
-                    let balance = await provider.getBalance("0xCD96f257Cc6603132Cf6B8709Ae14F0A391d1916")
-                    window.parent.postMessage({ type: 'metamaskResponse', data: balance }, parentUrl);
-
-                }
-            }
+              else if (data.type === 'metamaskRequest' && data.method === 'getBalance') {
+                let connectedAddress= await signer.getAddress();
+                console.log("Connected address: "+ connectedAddress)
+                let balance = await provider.getBalance(connectedAddress);
+                window.parent.postMessage({ type: 'metamaskResponse', data: balance }, parentUrl);
+              }
+              else if (data.type === 'metamaskRequest' && data.method === 'sendTransaction' && data.sendTo && data.value) {
+                let connectedAddress= await signer.getAddress();
+                // TODO : Error handling - value > 0 , address valid
+                let tx = await signer.sendTransaction({
+                    to: data.sendTo, 
+                    value: data.value
+                });
+                const receipt = await tx.wait();
+                window.parent.postMessage({ type: 'metamaskResponse', data: receipt }, parentUrl);
+              }
               
           }
       };
