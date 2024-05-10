063064ce06a493cf58e980935535a206f20766a4
Hira Siddiqui <12besehsiddiqui@seecs.edu.pk>
2024-05-11 00:52:54 +0200

    fix: exception handling in event listener

--- src/pages/presentation/auth/EventListener.tsx
+++ src/pages/presentation/auth/EventListener.tsx
@@ -1,99 +1,124 @@
 import { ethers, verifyMessage } from 'ethers';
-import { BrowserProvider, parseUnits } from "ethers";
-
 import React, { useEffect } from 'react';
-import { useAccount, useSignMessage } from 'wagmi';
 
 const EventListener: React.FC = () => {
     
       const receiveMessage = async (event: MessageEvent) => {
-          const parentUrl = "http://localhost:3001";
+          const parentUrl = process.env.REACT_APP_PARENT_URL;
           if (event.origin === parentUrl) {
               const data = event.data;
               let signer = null;
 
               let provider;
               if (window.ethereum == null) {
-                
-                // If MetaMask is not installed, we use the default provider,
-                // which is backed by a variety of third-party services (such
-                // as INFURA). They do not have private keys installed,
-                // so they only have read-only access
-                //console.log("MetaMask not installed; using read-only defaults")
-                //provider = ethers.getDefaultProvider()
                 console.log("Please install metamask");
-                return; //todo: send to parent?
-                
+                window.parent.postMessage({ type: 'noEthersProvider', data: "Please install metamask" }, parentUrl);
+                return;
               } else {
-                // Connect to the MetaMask EIP-1193 object. This is a standard
-                // protocol that allows Ethers access to make all read-only
-                // requests through MetaMask.
                 provider = new ethers.BrowserProvider(window.ethereum);
                 signer = await provider.getSigner();
               }
         
               if (data.type === 'metamaskRequest' && data.method === 'getAllAccounts') {
-                const accounts = await provider.listAccounts();
-                window.parent.postMessage({ type: 'metamaskResponse', data: accounts }, parentUrl);
-
+                try {
+                  const accounts = await provider.listAccounts();
+                  window.parent.postMessage({ type: 'metamaskResponse', data: accounts }, parentUrl);
+                }
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
+                }
               }
               else if (data.type === 'metamaskRequest' && data.method === 'getConnectedAccount') {
-                const accounts = await provider.listAccounts();
-                window.parent.postMessage({ type: 'metamaskResponse', data: accounts[0] }, parentUrl);
+                try {
+                  const accounts = await provider.listAccounts();
+                  window.parent.postMessage({ type: 'metamaskResponse', data: accounts[0] }, parentUrl);
+                }
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
+                }
               }
                             
               else if (data.type === 'metamaskRequest' && data.method === 'getMessageSignature' && data.message) {
-                      try {
-                      let signature = await signer.signMessage(data.message);
-                      window.parent.postMessage({ type: 'metamaskResponse', data: signature }, parentUrl);
-                      }
-                      catch (e) {
-                        console.log(e);
-                      }
-                  
+                try {
+                  let signature = await signer.signMessage(data.message);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: signature }, parentUrl);
+                }
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
+                }  
               }
               else if (data.type === 'metamaskRequest' && data.method === 'verifyMessageSignature' && data.signature && data.message) {
                 try {
-                let signerAddress = verifyMessage(data.message, data.signature);
-                if (signerAddress == await signer.getAddress()) {
-                  window.parent.postMessage({ type: 'metamaskResponse', data: "true" }, parentUrl);
+                  let signerAddress = verifyMessage(data.message, data.signature);
+                  if (signerAddress == await signer.getAddress()) {
+                    window.parent.postMessage({ type: 'metamaskResponse', data: "true" }, parentUrl);
+                  }
+                  else {
+                    window.parent.postMessage({ type: 'metamaskResponse', data: "false" }, parentUrl);
+                  }
                 }
-                else {
-                  window.parent.postMessage({ type: 'metamaskResponse', data: "false" }, parentUrl);
-                 }
-                }
-                catch (e) {
-                  console.log(e);
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
                 }
               }
               else if (data.type === 'metamaskRequest' && data.method === 'getBalance') {
-                let connectedAddress= await signer.getAddress();
-                console.log("Connected address: "+ connectedAddress)
-                let balance = await provider.getBalance(connectedAddress);
-                window.parent.postMessage({ type: 'metamaskResponse', data: balance.toString() + 'n' }, parentUrl);
+                try {
+                  let connectedAddress= await signer.getAddress();
+                  console.log("Connected address: "+ connectedAddress)
+                  let balance = await provider.getBalance(connectedAddress);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: balance.toString() + 'n' }, parentUrl);
+                }
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
+                }
               }
               else if (data.type === 'metamaskRequest' && data.method === 'sendTransaction' && data.sendTo && data.value) {
-                let connectedAddress= await signer.getAddress();
-                // TODO : Error handling - value > 0 , address valid
-                let tx = await signer.sendTransaction({
-                    to: data.sendTo, 
-                    value: data.value
-                });
-                const receipt = await tx.wait();
-                window.parent.postMessage({ type: 'metamaskResponse', data: receipt }, parentUrl);
+                try {
+                  //let connectedAddress= await signer.getAddress();
+                  // TODO : Error handling - value > 0 , address valid
+                  let tx = await signer.sendTransaction({
+                      to: data.sendTo, 
+                      value: data.value
+                  });
+                  const receipt = await tx.wait();
+                  console.log(receipt);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: receipt!.toString() }, parentUrl);
+                }
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
+                }
               }
               else if (data.type === 'metamaskRequest' && data.method === 'getBlockNumber' ) {
-                const blockNumber = await provider.getBlockNumber();
-                window.parent.postMessage({ type: 'metamaskResponse', data: blockNumber }, parentUrl);
+                try {
+                  const blockNumber = await provider.getBlockNumber();
+                  window.parent.postMessage({ type: 'metamaskResponse', data: blockNumber }, parentUrl);
+                }
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
+                }
               }
               else if (data.type === 'metamaskRequest' && data.method === 'getTransactionCount' && data.address ) {
-                const transactionCount = await provider.getTransactionCount(data.address);
-                window.parent.postMessage({ type: 'metamaskResponse', data: transactionCount }, parentUrl);
+                try {
+                  const transactionCount = await provider.getTransactionCount(data.address);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: transactionCount }, parentUrl);
+                }
+                catch (error: any) {
+                  console.error(error);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
+                }
               }
               else if (data.type === 'metamaskRequest' && data.method === 'readFromContract' && data.contractAddress && data.abi && data.methodName) {
                 // to be implemented
                 const contract = new ethers.Contract(data.contractAddress, data.abi,provider);
                 if (!contract[data.methodName]) {
+                  window.parent.postMessage({ type: 'metamaskResponse', data: "Method name does not exist on this contract" }, parentUrl);
                   throw new Error(`Method ${data.methodName} does not exist on the contract.`);
                 }
                 try {
@@ -104,14 +129,16 @@ const EventListener: React.FC = () => {
                     else {
                       result = await contract[data.methodName](...data.methodParams);
                     }
-                    window.parent.postMessage({ type: 'metamaskResponse', data: result }, parentUrl);
-                  } catch (error) {
-                    window.parent.postMessage({ type: 'metamaskResponse', data: error }, parentUrl);
+                    window.parent.postMessage({ type: 'metamaskResponse', data: result.toString() }, parentUrl);
+                  } catch (error: any) {
+                    console.error(error);
+                    window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
                 }
               }
               else if (data.type === 'metamaskRequest' && data.method === 'writeToContract' && data.contractAddress && data.abi && data.methodName ) {
                 const contract = new ethers.Contract(data.contractAddress, data.abi,signer);
                 if (!contract[data.methodName]) {
+                  window.parent.postMessage({ type: 'metamaskResponse', data: "Method name does not exist on this contract" }, parentUrl);
                   throw new Error(`Method ${data.methodName} does not exist on the contract.`);
                 }
                 try {
@@ -122,22 +149,21 @@ const EventListener: React.FC = () => {
                   else {
                     result = await contract[data.methodName](...data.methodParams);
                   }
-                  window.parent.postMessage({ type: 'metamaskResponse', data: result }, parentUrl);
-                  } catch (error) {
-                    window.parent.postMessage({ type: 'metamaskResponse', data: error }, parentUrl);
+                  window.parent.postMessage({ type: 'metamaskResponse', data: JSON.stringify(result) }, parentUrl);
+                  } catch (error: any) {
+                    console.error(error);
+                    window.parent.postMessage({ type: 'metamaskResponse', data: error.toString() }, parentUrl);
                 }
               }
             }
       };
   useEffect(() => {
-
     window.addEventListener('message', receiveMessage, false);
     // Cleanup function to remove the event listener
     return () => {
       window.removeEventListener('message', receiveMessage);
     };
   }, []); // Empty dependency array ensures the effect runs only once
-
   return <div></div>;
 };
 
