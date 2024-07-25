b45f5b16a5969e73f6e4b3de06e02c4c9d02a118
Zabeeh Ullah <zullah.bese15seecs@seecs.edu.pk>
2024-07-25 20:11:01 +0500

    feat: ui improvements

--- src/layout/Content/ContentRoutes.tsx
+++ src/layout/Content/ContentRoutes.tsx
@@ -1,20 +1,11 @@
 import React, { lazy, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import contents from '../../routes/contentRoutes';
-import WidgetAppHeader from '../Header/WidgetAppHeader';
 
 const PAGE_404 = lazy(() => import('../../pages/presentation/auth/Page404'));
 const ContentRoutes = () => {
-	const [showHeader, setShowHeader] = useState(false)
-
-
-	useEffect(() => {
-		const isHeaderVisible = localStorage.getItem('username') !== null
-		setShowHeader(isHeaderVisible)
-	}, [localStorage.getItem('username')])
 	return (
 		<>
-			{showHeader && <WidgetAppHeader />}
 			<Routes>
 				{contents.map((page) => (
 					// eslint-disable-next-line react/jsx-props-no-spreading
